75162d7bfcb63828b88cf71979284a55de9eba28
piepie22 <pieczynskimandy@yahoo.com>
2025-02-18 23:58:11 -0500

    1

---
+++ reactexerciseapplication copy/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react"; //usestate
+import "./design.css"; //importing my css
+import RepetitionExercise from "./components/RepetitionExercise"; //file paths
+import DurationExercise from "./components/DurationExercise"; //file paths
+//exercises
+const exercises = [
+  { name: "Push-Ups", type: "repetition" },
+  { name: "Planks", type: "duration" },
+  { name: "Running", type: "duration" },
+  { name: "Sit-Ups", type: "repetition" },
+  { name: "Squats", type: "repetition" },
+];
+function App() {
+  const [exercise, setExercise] = useState(null);
+  const exerciseComps = {
+    repetition: RepetitionExercise,
+    duration: DurationExercise,
+  };
+  const onReturn = () => setExercise(null);
+  if (exercise) {
+    const ExerciseComp = exerciseComps[exercise.type];
+    return (
+      <div>
+        <h2>{exercise.name}</h2>
+        <ExerciseComp name={exercise.name} />
+        <button onClick={onReturn}>Return</button>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>Choose an Exercise:</h1>
+      {exercises.map((exercise) => (
+        <button className="appjs-button" key={exercise.name}onClick={() => setExercise(exercise)}>{exercise.name}</button>
+      ))}
+    </div> );
+}
+export default App;
